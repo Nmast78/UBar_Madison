4b5d0f17e5a32dcd5c315c6d3f544a71789f33f0
Nick Mast <nicmast22@gmail.com>
2023-08-10 21:58:22 -0500

    Add button functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to go from homepage to add your experience page
+function goToAddPage(url) {
+    window.location.href = "addBar.html"
+}
+
+// Function to go from add your experience page to homepage
+function goToHomePage(url) {
+    window.location.href = "home.html"
+}
