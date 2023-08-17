184994de955eae451b91fe5398ff75359fb5ca6b
Nick Mast <nicmast22@gmail.com>
2023-08-16 22:03:39 -0500

    Test

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Function to go from homepage to add your experience page
 function goToAddPage(url) {
     window.location.href = "addBar.html"
-    
 }
 
 // Function to go from add your experience page to homepage
