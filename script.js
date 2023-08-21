bb05225cd70bc4d4289f57a98fe5fd144eaf81dd
quint@jettz <quintonjon16@gmail.com>
2023-08-21 00:28:02 -0500

    lots of changes

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function openPopup() {
     let popup = document.getElementById("popup");
     popup.classList.add("open-popup")
 }
+
+// Function that updates amount of minutes as user drags time slider
+function updateTimeSlider() {
+const waitMinutesElement = document.getElementById("wait-minutes");
+const sliderElement = document.getElementById("myRange");
+
+// Add an event listener to the slider to update the wait time text
+sliderElement.addEventListener("input", function () {
+    // Get the selected time interval from the slider's value
+    const selectedTime = sliderElement.value;
+
+    // Update the text content of the waitMinutesElement
+    waitMinutesElement.textContent = selectedTime === "0" ? "0" : selectedTime;
+});
+}
