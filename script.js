d16a54aefc6f9273120fb9e553d76568b2c06a1d
Nick Mast <nicmast22@gmail.com>
2023-09-01 07:59:09 -0500

    JS check dropdown function

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function goToHomePage(url) {
     window.location.href = "home.html"
 }
 
+
+function validate() {
+    var bar_name = document.getElementById("bars");
+    if(bar_name.selectedIndex <= 0) {
+        alert("Please Select a Bar");
+    } else {
+        openPopup();
+    }
+}
+
 // Function to open popup after user adds bar experience
 function openPopup() {
     let popup = document.getElementById("popup");
