fc7fc4bbaab712ac7cd796aa8daa18224811780d
Nick Mast <nicmast22@gmail.com>
2023-09-05 18:56:47 -0500

    Create new php file

---
+++ acceptanceRate.php
@@ -0,0 +1,12 @@
+<?php
+
+    // Connect to database
+    $host = "srv766.hstgr.io";
+    $dbUsername = "u304514465_root";
+    $dbPassword = "Tickmarble236!";
+    $dbName = "u304514465_got_in";
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+
+
+?>
