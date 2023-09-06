a7844bf8f291df732916e193391d6e878f38fe36
NickM <nicmast22@gmail.com>
2023-09-06 16:24:47 -0500

    php

---
+++ userInput.php
@@ -0,0 +1,10 @@
+<?php
+
+    // Connect to database
+    $host = "srv766.hstgr.io";
+    $dbUsername = "u304514465_root3";
+    $dbPassword = "Tickmarble236!";
+    $dbName = "u304514465_user_input";
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+?>
